b273ce58337d0a2949fc64d14bfce5ea3b707c3e
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-05-31 22:59:34 -0300

    feat: Models de Price

---
+++ backend/database/migrations/2025_06_01_015847_create_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id')->constrained();
+            $table->decimal('price', 10, 2);
+            $table->date('effective_date');
+            $table->decimal('occupancy_rate', 5, 2);
+            $table->boolean('is_holiday_or_weekend')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prices');
+    }
+};
