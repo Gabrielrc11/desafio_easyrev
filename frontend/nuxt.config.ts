df19abf251cb7260f053b211c075595cc4076848
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-06-02 22:37:36 -0300

    feat: Configurações de dependências

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  modules: ['@pinia/nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
